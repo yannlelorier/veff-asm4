f8f599301178791561424ad731c11340d4e49125
yannlelorier <yannllb97@gmail.com>
2021-03-31 12:36:55 +0000

    getting some progress with value checks

--- boardBackend/test/index.test.js
+++ boardBackend/test/index.test.js
@@ -44,15 +44,24 @@ describe('Endpoint tests', () => {
             .end(function(err, res) {
                 expect(res).to.have.status(200);
                 res.should.to.be.json;
-                expect(res.body).to.have.property("id");
-                expect(res.body).to.have.property("name");
-                expect(res.body).to.have.property("description");
+                //TODO how to check for no extra attibutes
+                expect(res.body).to.have.property("id").equal("0");
+                expect(res.body).to.have.property("name").equal("Planned");
+                expect(res.body).to.have.property("description").equal("My todo list.");
                 expect(res.body).to.have.property("tasks");
+                //check the task associated w that board
+                //TODO this shit doesn't work: (how to check for extra attributes)
+                expect(res.body.tasks).to.have.property("id");
+                expect(res.body.tasks).to.have.property("boardId");
+                expect(res.body.tasks).to.have.property("taskName");
+                expect(res.body.tasks).to.have.property("dateCreated");
+                expect(res.body.tasks).to.have.property("archived");
+
                 done();
             })
     })
 
-    //POST /api/v1/boards/:boardId
+    //POST /api/v1/boards/
     it('POST request for a board - success case', function(done) {
         let testBoard = {
             name:'TestBoard',
@@ -65,10 +74,17 @@ describe('Endpoint tests', () => {
             .end(function(err, res) {
                 res.should.have.status(201);
                 res.should.to.be.json;
+                //TODO check for no extra attributes
+                //TODO check for correct values
+                expect(res.body).to.have.property("id");
+                expect(res.body).to.have.property("name");
+                expect(res.body).to.have.property("description");
+                expect(res.body).to.have.property("tasks");
                 done();
             })
     })
-
+    
+    //PUT /api/v1/boards/:boardId S
     it('PUT request for a board (id=1) - success case', function(done) {
         let testBoard = {
             name: 'TestName',
@@ -82,10 +98,17 @@ describe('Endpoint tests', () => {
             .end(function(err, res) {
                 res.should.have.status(200);
                 res.should.to.be.json;
+                //TODO check no extra attributes
+                //TODO check correct values
+                expect(res.body).to.have.property("id");
+                expect(res.body).to.have.property("name");
+                expect(res.body).to.have.property("description");
+                expect(res.body).to.have.property("tasks");
                 done();
             })
     })
 
+    // PUT /api/v1/boards/:boardId F
     it('PUT request for a board (id=1) - Failing case', function(done) {
         let badTestBoard = {
             name: 'TestBoard'
@@ -103,6 +126,7 @@ describe('Endpoint tests', () => {
             })
     })
 
+    //GET /api/v1/boards/:boardId/tasks
     it('GET request for the tasks of a specific board (id=0)', function(done) {
         chai.request(apiUrl)
             .get('/api/v1/boards/0/tasks')
@@ -113,6 +137,27 @@ describe('Endpoint tests', () => {
                 done();
             })
     })
+
+    //GET /api/v1/boards/:boardId/tasks/:taskId
+    it('GET request for a specific task (boardId=0, taskId=0)', function(done) {
+        chai.request(apiUrl)
+            .get('/api/v1/boards/0/tasks/0')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                res.should.to.be.json;
+                //TODO how to check for no extra properties
+                //TODO how to check for the right values
+                expect(res.body).to.have.property("id");
+                expect(res.body).to.have.property("boardId");
+                expect(res.body).to.have.property("taskName");
+                expect(res.body).to.have.property("dateCreated");
+                expect(res.body).to.have.property("archived");
+                done();
+            })
+    })
+
+    
+
 
     // // This test doesn't do much
     // it("Please remove me before handin - I don't do much", function (done) {
